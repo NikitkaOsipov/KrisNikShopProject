7c6ec3ea2b7482b0cb50601175adbd581bfaedff
NikitkaOsipov <120587209+NikitkaOsipov@users.noreply.github.com>
2024-03-21 11:44:45 +0000

    Fixed profile button. Added profile page + adding profile page (21.03.2024)

--- KrisNikShopProject/Components/Data/UserModel.cs
+++ KrisNikShopProject/Components/Data/UserModel.cs
@@ -7,24 +7,39 @@ namespace KrisNikShopProject.Components.Data
         public string? Name { get; set; }
 		public string? Password { get; set; }
 		public string? Email { get; set; }
-
         public string? Role { get; set; } = "User";
+        public DateTime? DateOfCreating { get; }
+        public string? PhoneNumber { get; set; }
+
 		public UserModel()
         {
+            DateOfCreating = DateTime.Now;
 		}
 
-		public UserModel(int id, string name, string password, string email, string role)
+		public UserModel(int id, string name, string password, string email, string role, DateTime dateOfCreating, string phoneNumber)
         {
             Id = id;
             Name = name;
             Password = password;
             Email = email;
             Role = role;
+            DateOfCreating = dateOfCreating;
+            PhoneNumber = phoneNumber;
+        }
+
+        public UserModel(int id, string name, string password, string email, string role)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+            Email = email;
+            Role = role;
+            DateOfCreating = DateTime.Now;
         }
 
         public override string ToString()
         {
-			return  Id + "," + Name + "," + Password + "," + Email + "," + Role;
+			return  Id + "," + Name + "," + Password + "," + Email + "," + Role + "," + DateOfCreating + "," + (PhoneNumber ?? "Unknown");	
 		}
     }
 }
