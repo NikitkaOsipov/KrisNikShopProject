aae38064ec0d0a8853de6c8051b4758afb7ec66f
Kristians Sapožņikovs <119543405+GoldenX28@users.noreply.github.com>
2024-04-04 10:38:38 +0000

    Added a page for products details, small ajdjstments  to ProductModel

--- KrisNikShopProject/Components/Data/ProductModel.cs
+++ KrisNikShopProject/Components/Data/ProductModel.cs
@@ -3,21 +3,23 @@
     public class ProductModel
     {
         public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int? DistanceToLatvia { get; set; }
+        public double Price { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
         public string? Category { get; set; }
         public string? Brand { get; set; }
         public string? Size { get; set; }
         public string? Material { get; set; }
+        public string? Country { get; set; }
         public string? Image { get; set; }
 
         public ProductModel()
         {
         }
 
-        public ProductModel(int id, string name, string description, double price, int quantity, string category, string brand, string size, string material, string image)
+        public ProductModel(int id, string name, string description, double price, int quantity, string category, string brand, string size, string material, string country, int distanceToLatvia, string image)
         {
             Id = id;
             Name = name;
@@ -29,6 +31,8 @@
             Size = size;
             Material = material;
             Image = image;
+            Country = country;
+            DistanceToLatvia = distanceToLatvia;
         }
 
         public override string ToString()
--- KrisNikShopProject/Components/Data/ProductStorageService.cs
+++ KrisNikShopProject/Components/Data/ProductStorageService.cs
@@ -22,12 +22,26 @@
                     string[] parts = line.Split(',');
                     ProductModel product = new ProductModel(
                                                int.Parse(parts[0]), parts[1], parts[2], double.Parse(parts[3]), int.Parse(parts[4]), 
-                                               parts[5], parts[6], parts[7], parts[8], parts[9]);
+                                               parts[5], parts[6], parts[7], parts[8], parts[9], int.Parse(parts[10] ?? "0"), parts[11]);
                     products.Add(product);
                 }
             }
 
             return products;
+        }
+
+        public ProductModel? GetProductById(int id)
+        {
+            ProductModel? productID = null;
+            List<ProductModel> products = GetAllProducts();
+            foreach(ProductModel product in products)
+            {
+                if (id == product.Id)
+                {
+                    productID = product;
+                }
+            }
+            return productID;
         }
     }
 }
