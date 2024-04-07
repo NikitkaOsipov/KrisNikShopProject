e34433b934ec1ca24ecaeb2aaa5869db7206b845
Nikita <no00499@rvt.lv>
2024-04-07 19:06:40 +0300

    Adding CartPage, some buttons, new Icons

--- KrisNikShopProject/Components/Data/CartStorageService.cs
+++ KrisNikShopProject/Components/Data/CartStorageService.cs
@@ -3,10 +3,12 @@
     public class CartStorageService
     {
         private readonly UserRegistrationService userRegistrationService;
-
-        public CartStorageService(UserRegistrationService userRegistrationService)
+        private readonly ProductStorageService productStorageService;
+         
+        public CartStorageService(UserRegistrationService userRegistrationService, ProductStorageService productStorageService)
         {
             this.userRegistrationService = userRegistrationService;
+            this.productStorageService = productStorageService;
             //In this code, the CartStorageService constructor takes a UserRegistrationService parameter,
             //which is provided by the dependency injection system. The parameter is assigned to the _userRegistrationService field,
             //which can then be used throughout the CartStorageService class.
@@ -29,6 +31,29 @@
                     sw.WriteLine($"{product.Id},{product.Name}"); 
                 }
             }
+        }
+
+        public List<ProductModel> GetAllProducts()
+        {
+            List<ProductModel> products = new List<ProductModel>();
+
+            string fileName = $"C:\\Users\\osipo\\BlazorProjects\\KrisNikShopProject\\KrisNikShopProject\\Components\\Collections\\UsersCarts\\{userRegistrationService.CurrentUser.Id}_cart.csv";
+
+            if (File.Exists(fileName))
+            {
+                using (var reader = new StreamReader(fileName))
+                {
+                    string? line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        ProductModel product = productStorageService.GetProductById(int.Parse(parts[0]));
+                        products.Add(product);
+                    }
+                }
+            }
+
+            return products;
         }
     }
 }
